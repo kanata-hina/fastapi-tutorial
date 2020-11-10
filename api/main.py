213ea1b51e1a3c72500444f3eeb3d5f269838799
kanata <kanata@teamj.dev>
2020-11-11 06:49:11 +0900

    返り値修正

--- api/main.py
+++ api/main.py
@@ -3,14 +3,17 @@ from pydantic import BaseModel
 import os
 import psycopg2
 
-DATABASE_URL='postgresql://admin:admin@db:5432/db'
+DATABASE_URL = 'postgresql://admin:admin@db:5432/db'
+
 
 def get_connection():
     return psycopg2.connect(DATABASE_URL)
 
+
 class Task(BaseModel):
     title: str
     description: str
+
 
 api = FastAPI()
 
@@ -19,26 +22,32 @@ api = FastAPI()
 async def site_root():
     """root"""
     cursor = psycopg2.connect(DATABASE_URL)
-    #print(cursor)
+    # print(cursor)
     return {"message": "Hello, WORLD!"}
 
+
 @api.get('/{id}/tasks')
-async def getAllTasks(id: int):
+async def getAllTasks(id: str):
     conn = get_connection()
     cur = conn.cursor()
-    cur.execute('SELECT * FROM task where user_id = ' + str(id))
+    cur.execute('SELECT * FROM task where user_id = ' + id)
     rows = cur.fetchall()
-    #print(rows)
+    if len(rows) == 0:
+        raise HTTPException(status_code=500, detail="task id not found")
+    tasks = [{"task_id": row[0], "title": row[2], "description": row[3]}
+             for row in rows]
     cur.close()
     conn.close()
-    return {"item_id": id}
+    return tasks
+
 
 @api.post('/{id}/tasks')
 async def createTask(id: int, task: Task):
     print(task)
     conn = get_connection()
     cur = conn.cursor()
-    cur.execute("INSERT INTO task(user_id, title, description) VALUES(" + str(id) + ", '" + task.title + "', '" + task.description + "')")
+    cur.execute("INSERT INTO task(user_id, title, description) VALUES(" +
+                str(id) + ", '" + task.title + "', '" + task.description + "')")
     conn.commit()
     cur.execute('SELECT * FROM task')
     rows = cur.fetchall()
@@ -47,11 +56,13 @@ async def createTask(id: int, task: Task):
     conn.close()
     return {"item_id": id}
 
+
 @api.get('/{id}/tasks/{task_id}')
 async def getAllTasks(id: int, task_id: int):
     conn = get_connection()
     cur = conn.cursor()
-    cur.execute('SELECT * FROM task where user_id = ' + str(id) + ' and id = ' + str(task_id))
+    cur.execute('SELECT * FROM task where user_id = ' +
+                str(id) + ' and id = ' + str(task_id))
     rows = cur.fetchall()
     if len(rows) == 0:
         raise HTTPException(status_code=500, detail="task id not found")
@@ -60,18 +71,22 @@ async def getAllTasks(id: int, task_id: int):
     conn.close()
     return {"task_id": rows[0][0], "title": rows[0][2], "description": rows[0][3]}
 
+
 @api.put('/{id}/tasks/{task_id}')
 async def getAllTasks(id: int, task_id: int, task: Task):
     conn = get_connection()
     cur = conn.cursor()
-    cur.execute("UPDATE task SET title = '" + task.title + "', description = '" + task.description + "' where id = " + str(task_id) + " and user_id = " + str(id))
+    cur.execute("UPDATE task SET title = '" + task.title + "', description = '" +
+                task.description + "' where id = " + str(task_id) + " and user_id = " + str(id))
     conn.commit()
-    cur.execute('SELECT * FROM task where user_id = ' + str(id) + ' and id = ' + str(task_id))
+    cur.execute('SELECT * FROM task where user_id = ' +
+                str(id) + ' and id = ' + str(task_id))
     rows = cur.fetchall()
     print(rows)
     cur.close()
     conn.close()
     return {"item_id": id}
+
 
 @api.delete('/{id}/tasks')
 async def getAllTasks(id: int):
