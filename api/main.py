337bfce12896711c27271c64191e0282bc34eb63
村松 啓寛 <muramatsu.akihiro@nifty.co.jp>
2020-11-11 16:34:40 +0900

    APIfix

--- api/main.py
+++ api/main.py
@@ -30,10 +30,14 @@ async def site_root():
 async def getAllTasks(id: str):
     conn = get_connection()
     cur = conn.cursor()
-    cur.execute('SELECT * FROM task where user_id = ' + id)
+    cur.execute("SELECT * FROM id where id = '" + id + "'")
     rows = cur.fetchall()
     if len(rows) == 0:
-        raise HTTPException(status_code=500, detail="task id not found")
+        raise HTTPException(status_code=500, detail="user id not found")
+    cur.execute("SELECT * FROM task where user_id = '" + id + "'")
+    rows = cur.fetchall()
+    if len(rows) == 0:
+        raise HTTPException(status_code=500, detail="task not found")
     tasks = [{"task_id": row[0], "title": row[2], "description": row[3]}
              for row in rows]
     cur.close()
@@ -42,61 +46,72 @@ async def getAllTasks(id: str):
 
 
 @api.post('/{id}/tasks')
-async def createTask(id: int, task: Task):
-    print(task)
+async def createTask(id: str, task: Task):
     conn = get_connection()
     cur = conn.cursor()
-    cur.execute("INSERT INTO task(user_id, title, description) VALUES(" +
-                str(id) + ", '" + task.title + "', '" + task.description + "')")
-    conn.commit()
-    cur.execute('SELECT * FROM task')
+    cur.execute("SELECT * FROM id where id = '" + id + "'")
     rows = cur.fetchall()
-    print(rows)
+    if len(rows) == 0:
+        raise HTTPException(status_code=500, detail="user id not found")
+    cur.execute("INSERT INTO task(user_id, title, description) VALUES('" + id + "', '" + task.title + "', '" + task.description + "') RETURNING id, title, description")
+    conn.commit()
+    task = cur.fetchone()
+    if len(task) == 0:
+        raise HTTPException(status_code=500, detail="task can not create")
     cur.close()
     conn.close()
-    return {"item_id": id}
+    return {"task_id": task[0], "title": task[1], "description": task[2]}
 
 
 @api.get('/{id}/tasks/{task_id}')
-async def getAllTasks(id: int, task_id: int):
+async def getAllTasks(id: str, task_id: int):
     conn = get_connection()
     cur = conn.cursor()
-    cur.execute('SELECT * FROM task where user_id = ' +
-                str(id) + ' and id = ' + str(task_id))
+    cur.execute("SELECT * FROM id where id = '" + id + "'")
+    rows = cur.fetchall()
+    if len(rows) == 0:
+        raise HTTPException(status_code=500, detail="user id not found")
+    cur.execute("SELECT * FROM task where user_id = '" + id + "' and id = " + str(task_id))
     rows = cur.fetchall()
     if len(rows) == 0:
         raise HTTPException(status_code=500, detail="task id not found")
-    print(rows)
     cur.close()
     conn.close()
     return {"task_id": rows[0][0], "title": rows[0][2], "description": rows[0][3]}
 
 
 @api.put('/{id}/tasks/{task_id}')
-async def getAllTasks(id: int, task_id: int, task: Task):
+async def getAllTasks(id: str, task_id: int, task: Task):
     conn = get_connection()
     cur = conn.cursor()
-    cur.execute("UPDATE task SET title = '" + task.title + "', description = '" +
-                task.description + "' where id = " + str(task_id) + " and user_id = " + str(id))
-    conn.commit()
-    cur.execute('SELECT * FROM task where user_id = ' +
-                str(id) + ' and id = ' + str(task_id))
+    cur.execute("SELECT * FROM id where id = '" + id + "'")
     rows = cur.fetchall()
-    print(rows)
+    if len(rows) == 0:
+        raise HTTPException(status_code=500, detail="user id not found")
+    cur.execute("SELECT * FROM task where id = " + str(task_id))
+    rows = cur.fetchall()
+    if len(rows) == 0:
+        raise HTTPException(status_code=500, detail="task id not found")
+    cur.execute("UPDATE task SET title = '" + task.title + "', description = '" + task.description + "' where id = " + str(task_id) + " and user_id = '" + id + "' RETURNING title, description")
+    conn.commit()
+    task = cur.fetchone()
+    if len(task) == 0:
+        raise HTTPException(status_code=500, detail="task can not create")
     cur.close()
     conn.close()
-    return {"item_id": id}
+    return {"title": task[0], "description": task[1]}
 
 
 @api.delete('/{id}/tasks')
-async def getAllTasks(id: int):
+async def getAllTasks(id: str):
     conn = get_connection()
     cur = conn.cursor()
-    cur.execute("DELETE from task where user_id = " + str(id))
-    conn.commit()
-    cur.execute('SELECT * FROM task where user_id = ' + str(id))
+    cur.execute("SELECT * FROM id where id = '" + id + "'")
     rows = cur.fetchall()
-    print(rows)
+    if len(rows) == 0:
+        raise HTTPException(status_code=500, detail="user id not found")
+    cur.execute("DELETE from task where user_id = '" + id + "'")
+    conn.commit()
     cur.close()
     conn.close()
-    return {"item_id": id}
+    return {}
--- db/init.sql
+++ db/init.sql
@@ -1,13 +1,13 @@
 create table id (
-  id integer PRIMARY KEY, 
+  id TEXT PRIMARY KEY, 
   name varchar(10)
 );
 
 create table task (
   id serial PRIMARY KEY, 
-  user_id integer REFERENCES id(id), 
+  user_id TEXT REFERENCES id(id), 
   title TEXT,
   description TEXT
 );
 
-INSERT INTO id(id, name) VALUES(01967, 'sci01967');
+INSERT INTO id(id, name) VALUES('sci01967', 'sci01967');
